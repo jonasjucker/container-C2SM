1a91432ee12be3c4898248dea27fc7e586785c99
juckerj <jonas.jucker@env.ethz.ch>
2020-10-05 17:45:34 +0200

    cosmo Dockerfile

---
+++ cosmo_cpu/clone_repositories.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Start cloning repos..."
+workdir=$(pwd)
+
+git clone git@github.com:C2SM-RCM/libgrib1.git
+
+git clone --branch libgrib_api_1.20.0.2 git@github.com:C2SM-RCM/libgrib-api-vendor.git libgrib-api
+
+git clone --branch v1.20.0.2 git@github.com:C2SM-RCM/libgrib-api-cosmo-resources.git cosmo_definitions
+
+git clone git@github.com:C2SM-RCM/libjasper.git
+
+git clone --branch docker git@github.com:jonasjucker/cosmo.git
+
+echo "***All repos cloned***"
+
