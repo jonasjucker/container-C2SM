c8d0460504d7c925dae14751b24354e9b579ab26
juckerj <jonas.jucker@env.ethz.ch>
2020-10-07 15:51:25 +0200

    update grib_api

--- cosmo_cpu/clone_repositories.sh
+++ cosmo_cpu/clone_repositories.sh
@@ -7,7 +7,7 @@ git clone git@github.com:C2SM-RCM/libgrib1.git
 
 git clone --branch libgrib_api_1.20.0.2 git@github.com:C2SM-RCM/libgrib-api-vendor.git libgrib-api
 
-git clone --branch v1.20.0.2 git@github.com:C2SM-RCM/libgrib-api-cosmo-resources.git cosmo_definitions
+git clone --branch v1.20.0.3 git@github.com:C2SM-RCM/libgrib-api-cosmo-resources.git cosmo_definitions
 
 git clone git@github.com:C2SM-RCM/libjasper.git
 
