fe98982d779bb4c9f01e5a6d5d5372a9af48518f
juckerj <jonas.jucker@env.ethz.ch>
2020-10-12 16:50:12 +0200

    Gridtools works, configure of Dycore fails -> needs hack in SerialboxCMakeFile

---
+++ cosmo_gpu/clone_repositories.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "Start cloning repos..."
+workdir=$(pwd)
+
+git clone git@github.com:C2SM-RCM/libgrib1.git
+
+git clone --branch libgrib_api_1.20.0.2 git@github.com:C2SM-RCM/libgrib-api-vendor.git libgrib-api
+
+git clone --branch v1.20.0.2 git@github.com:C2SM-RCM/libgrib-api-cosmo-resources.git cosmo_definitions
+
+git clone git@github.com:C2SM-RCM/libjasper.git
+
+git clone --branch docker git@github.com:jonasjucker/cosmo.git
+
+git clone git@github.com:GridTools/gridtools.git
+cd gridtools
+git checkout  8101c64
+cd ..
+
+git clone git@github.com:GridTools/serialbox.git
+
+
+echo "***All repos cloned***"
+
