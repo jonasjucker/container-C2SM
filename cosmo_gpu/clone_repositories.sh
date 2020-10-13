53cccc1eaf0434e5ca8d66f3ffe9e413007038ad
juckerj <jonas.jucker@env.ethz.ch>
2020-10-13 11:59:37 +0200

    unify CPU

--- cosmo_gpu/clone_repositories.sh
+++ cosmo_gpu/clone_repositories.sh
@@ -20,6 +20,12 @@ cd ..
 
 git clone git@github.com:GridTools/serialbox.git
 
+wget https://confluence.ecmwf.int/download/attachments/45757960/eccodes-2.14.1-Source.tar.gz
+tar xpzf eccodes-2.14.1-Source.tar.gz && rm eccodes-2.14.1-Source.tar.gz
 
+git clone git@github.com:COSMO-ORG/eccodes-cosmo-resources.git
+cd eccodes-cosmo-resources
+git checkout 15f3a862d0349f4fc332e383c69acbed71b7804d
+cd ..
 echo "***All repos cloned***"
 
