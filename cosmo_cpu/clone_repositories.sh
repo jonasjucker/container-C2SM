bc12ebb371b5566fe5ea7e0dcbf3585aa0e8e5df
juckerj <jonas.jucker@env.ethz.ch>
2020-10-13 11:34:43 +0200

    only one cpu version left

--- cosmo_cpu/clone_repositories.sh
+++ cosmo_cpu/clone_repositories.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 echo "Start cloning repos..."
-workdir=$(pwd)
 
 git clone git@github.com:C2SM-RCM/libgrib1.git
-
-git clone --branch libgrib_api_1.20.0.2 git@github.com:C2SM-RCM/libgrib-api-vendor.git libgrib-api
-
-git clone --branch v1.20.0.3 git@github.com:C2SM-RCM/libgrib-api-cosmo-resources.git cosmo_definitions
 
 git clone git@github.com:C2SM-RCM/libjasper.git
 
 git clone --branch docker git@github.com:jonasjucker/cosmo.git
+
+wget https://confluence.ecmwf.int/download/attachments/45757960/eccodes-2.14.1-Source.tar.gz
+tar xpzf eccodes-2.14.1-Source.tar.gz && rm eccodes-2.14.1-Source.tar.gz
+
+git clone git@github.com:COSMO-ORG/eccodes-cosmo-resources.git
+cd eccodes-cosmo-resources
+git checkout 15f3a862d0349f4fc332e383c69acbed71b7804d
+cd ..
 
 echo "***All repos cloned***"
 
